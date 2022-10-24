276414075adb724210598f3d5c48bf137a7aebfd
SprintKeyz <63497433+SprintKeyz@users.noreply.github.com>
2022-10-23 23:48:01 -0400

    New UI with easily changeable accent colors and more fields

--- ui/views.py
+++ ui/views.py
@@ -6,3 +6,22 @@ from django.http import HttpResponse
 
 def home(request):
     return render(request, 'home.html', {'title': 'Report Card Commenter'})
+
+
+'''
+<h2>REPORT CREATOR</h2>
+        <div class="category" id="cat1">
+            <p>Student file</p>
+            <div id="uploadbox1" class="uploadbox">
+                <img src="{% static 'img/file.svg' %}" draggable="false">
+                <p>Click or drag files here to upload</p>
+            </div>
+        </div>
+
+        <hr>
+
+        <div class="buttons">
+            <button id="reset">Reset</button>
+            <button id="submit">Generate</button>
+        </div>
+'''
