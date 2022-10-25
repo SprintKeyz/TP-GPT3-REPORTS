3652043bf03b865460f1ad9592298f04166ec16c
SprintKeyz <63497433+SprintKeyz@users.noreply.github.com>
2022-10-25 13:17:54 -0400

    Prevent form resubmit on reload?
    Didn’t test it…

--- ui/views.py
+++ ui/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 # import handle_submit from form_submit.py
 from .form_submit import handle_submit
 # Create your views here.
@@ -8,5 +8,6 @@ def home(request):
     if request.method == "POST":
         print("Got form type", request.content_type)
         handle_submit(request.POST, request.FILES)
+        return redirect('')
 
     return render(request, 'home.html', {'title': 'Report Card Commenter'})
